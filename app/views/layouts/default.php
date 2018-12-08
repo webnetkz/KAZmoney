57d1f47e1a67bfb4792a80ec07f60888ccbd8bb3
qazaqlife <8gg001@gmail.com>
2018-12-08 20:33:26 +0600

    deleted old preloader

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -26,57 +26,16 @@
                 color: white;
                 margin: 5px;
             }
-
-            #hellopreloader>p{
-                display: none;
-            }
-
-
-            #preloader_webnet {
-                text-align: center;
-                margin: 10%;
-                color: white;
-                font-size: 4em;
-                text-shadow: 1px 2px 10px black;
-            }
-
-            #hellopreloader_preload {
-                display: block;
-                position: fixed;
-                z-index: 99999;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background-size:135px;
-                background: rgba(0, 0 , 0, 0.5 ) url(/public/img/preloader.svg) center center no-repeat;
-            }
-
         </style>
             
 
     </head>
     <body>
 	   
-    <div id="hellopreloader">
-        <div id="hellopreloader_preload">
-            <h1 id="preloader_webnet">WEB NET</h1>
-        </div>
-    </div>
+    
    
 
-        <div id=allItems>
-            <form>
-                <details>
-                    <summary>form</summary>
-                    <input type="text" name="">
-                    <option>web</option>
-                    <option>net</option>
-                </details>
-            </form>
-        </div>
-        <meter value="2">d</meter>
-        <progress value="33" max="100" style="width: 100%;">
+        
 
     <details>
         <summary>Hello php</summary>
@@ -92,16 +51,7 @@
         </footer>
     
 
-    <script type="text/javascript">
-        var hellopreloader = document.getElementById("hellopreloader_preload");
-        function fadeOutnojquery(el){el.style.opacity = 1;
-            var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;
-            if (el.style.opacity <=0.05){ 
-                clearInterval(interhellopreloader);
-                hellopreloader.style.display = "none";
-            }},16);
-        }window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
-    </script>
+    
     <script type="text/javascript" src="public/js/jquery.js"></script>
     <script type="text/javascript" src="public/js/date.js"></script>
     </body>
