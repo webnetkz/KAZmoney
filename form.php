cd864acaa26f7beed09fb7834a597f43a2501e35
webnetkz <8gg001@gmail.com>
2019-05-05 01:30:23 +0600

    update 05 may

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+
+    if(!empty($_POST['yourphone'])) {
+
+        $phone = trim($_POST['yourphone']);
+        $phone = htmlentities($phone);
+
+        $to      = 'dbqqbq@gmail.com';
+        $subject = 'Client';
+        $message = $phone;
+        $headers = 'From: info@webnet.kz' . "\r\n" .
+            'Reply-To: info@webnet.kz' . "\r\n" .
+            'X-Mailer: PHP/';
+
+        mail($to, $subject, $message, $headers);
+
+        header('Location: http://webnet/index.html');
+    }
