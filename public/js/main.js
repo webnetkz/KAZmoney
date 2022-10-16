802e9e8bda3c62e40f9690b8478283e45d51b922
webnetkz <dbqqbq@gmail.com>
2022-10-16 21:40:33 +0600

    update

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import { parallaxScroll } from './moduls/parallaxScroll.js';
-// import { showVisible } from './moduls/showOnScroll.js';
-
-// window.addEventListener('scroll', () => {
-
-// });
-
