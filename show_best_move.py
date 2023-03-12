d6128c19f3ff2fe7d9a342b3df1cf466277fc9d8
webnetkz <dbqqbq@gmail.com>
2023-03-13 03:51:08 +0600

    Minor change

--- show_best_move.py
+++ show_best_move.py
@@ -10,28 +10,6 @@ import cv2
 # BOARD_LEFT_COORD = position_board[0]+3
 
 
-from PyQt6 import QtGui
-from PyQt6.QtCore import Qt
-import win32gui
-import win32ui
-import win32con
-
-hwnd = win32gui.GetDesktopWindow()
-wDC = win32gui.GetWindowDC(hwnd)
-dc = win32ui.CreateDCFromHandle(wDC)
-
-painter = QtGui.QPainter()
-painter.begin(dc)
-
-pen = QtGui.QPen(Qt.red)
-pen.setWidth(5)
-painter.setPen(pen)
-
-painter.drawLine(0, 0, 500, 500)
-
-painter.end()
-dc.DeleteDC()
-win32gui.ReleaseDC(hwnd, wDC)
 
 
 
