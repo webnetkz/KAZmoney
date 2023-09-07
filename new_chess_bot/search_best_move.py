413f16ebdddb3e306aacaf17bc3c21dfa7005c9a
webnetkz <dbqqbq@gmail.com>
2023-09-07 16:13:03 +0600

    Update stockfish

--- new_chess_bot/search_best_move.py
+++ new_chess_bot/search_best_move.py
@@ -3,8 +3,8 @@ import chess.engine
 
 def search_best_move(fen_position):
     board = chess.Board(fen_position) 
-    engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
-    best_move = str(engine.play(board, chess.engine.Limit(time=0.3)).move)
+    engine = chess.engine.SimpleEngine.popen_uci("/mnt/c/Users/dbqqb/onchessbot/new_chess_bot/stockfish.exe")
+    best_move = str(engine.play(board, chess.engine.Limit(time=0.4)).move)
     
     engine.quit()
 
