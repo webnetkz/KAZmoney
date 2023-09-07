9b0735a1cbcd1d122ef83f0626b56e1b94c9b59d
webnetkz <dbqqbq@gmail.com>
2023-09-07 20:50:48 +0600

    Completed function search chess board

--- new_chess_bot/search_board.py
+++ new_chess_bot/search_board.py
@@ -1,40 +1,68 @@
-import cv2
 import numpy as np
-import pyautogui
+import cv2
+import pyautogui as pg
 
-def find_chessboard_on_screen():
-    # Задайте размер области, которую вы хотите сканировать (в пикселях)
-    screen_width, screen_height = pyautogui.size()
-    region = (0, 0, screen_width, screen_height)
 
-    # Загрузка каскада Хаара для шахматной доски
-    chessboard_cascade = cv2.CascadeClassifier('chessboard.xml')
+def get_start_position():
+    screen = './shot_board.png'
+    pg.screenshot(screen)
+    img = cv2.imread(screen)
 
-    while True:
-        # Захватите изображение с экрана в указанной области
-        screenshot = pyautogui.screenshot(region=region)
 
-        # Преобразовать скриншот в массив numpy
-        screen_img = np.array(screenshot)
+    # Преобразование изображения в массив numpy и перевод в серый цвет
+    img_np = np.array(img)
+    gray = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
 
-        # Преобразовать изображение в оттенки серого
-        gray_img = cv2.cvtColor(screen_img, cv2.COLOR_BGR2GRAY)
+    # Применение фильтра Гаусса для уменьшения шума на изображении
+    blurred = cv2.GaussianBlur(gray, (1, 1), 0)
 
-        # Найти шахматную доску на изображении
-        chessboards = chessboard_cascade.detectMultiScale(gray_img, scaleFactor=1.2, minNeighbors=5)
+    # Обнаружение горизонтальных и вертикальных линий на изображении
+    edges = cv2.Canny(blurred, 254, 255, apertureSize=3)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=50, minLineLength=100, maxLineGap=5)
 
-        # Если найдена хотя бы одна доска, вывести координаты
-        if len(chessboards) > 0:
-            for (x, y, w, h) in chessboards:
-                print(f"Шахматная доска найдена на координатах: x={x}, y={y}, ширина={w}, высота={h}")
-                # Вы можете выполнить другие действия с найденной доской здесь
+    matrix_img = create_clear_matrix()
 
-        # Если не найдено ни одной доски, вывести сообщение
-        if len(chessboards) == 0:
-            print("Шахматная доска не найдена на экране.")
+    # Рисование линий на изображении
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(img_np, (x1, y1), (x2, y2), (0, 0, 255), 1)
+        cv2.line(matrix_img, (x1, y1), (x2, y2), (0, 0, 255), 3)
 
-        # Ожидание некоторого времени перед следующей проверкой
-        cv2.waitKey(1000)  # Подождать 1 секунду
 
-if __name__ == "__main__":
-    find_chessboard_on_screen()
+    cv2.imwrite("./images/no_bg_board.png", matrix_img)
+    result_position_chess_board = find_chessboard_contour(matrix_img)
+
+    return result_position_chess_board
+
+# Возвращает x, y, w, h всей доски
+def find_chessboard_contour(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edges = cv2.Canny(blurred, 50, 200)
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        peri = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
+
+        if len(approx) == 4 and cv2.isContourConvex(approx):
+            x, y, w, h = cv2.boundingRect(contour)
+            aspect_ratio = float(w) / h
+
+            if 0.9 <= aspect_ratio <= 1.1:
+                return x, y, w, h
+
+    return None
+
+def create_clear_matrix():
+    # Получаем размеры экрана
+    screen_width, screen_height = pg.size()
+
+    # Создаем пустое черное изображение
+    black_image = np.zeros((screen_height, screen_width, 3), dtype=np.uint8)
+    return black_image
+
+
+while True:
+    print(get_start_position())
