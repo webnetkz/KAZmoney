9e0ff04814705795e665678c43f8045a74629bc1
webnetkz <dbqqbq@gmail.com>
2023-09-16 22:19:31 +0600

    Create clear branch for site

--- core_chessbot.py
+++
@@ -1,8 +0,0 @@
-from get_position import get_position
-from get_fen import get_fen
-from print_board import print_board
-from search_best_move import search_best_move
-
-fen = get_fen(get_position('https://lichess.org/uyic0uK9r1PT'))
-print(search_best_move(fen))
-
