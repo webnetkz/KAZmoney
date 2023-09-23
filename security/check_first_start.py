291eaef19b6a04786becea5f3047c7f1ac4bbac4
webnetkz <dbqqbq@gmail.com>
2023-09-23 22:26:38 +0600

    Update security script

--- security/check_first_start.py
+++ security/check_first_start.py
@@ -1,5 +1,11 @@
 import requests
+import json
 
-def first_start():
-    pass
 
+def first_start(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        response_json = response.text
+        print(response_json)
+
+first_start('https://xchess.webnet.kz/check-code?code=thisIsCode')
