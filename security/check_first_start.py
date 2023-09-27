b26afd0da659534b0ff9a9b930aa5d9bfaecbf9d
webnetkz <dbqqbq@gmail.com>
2023-09-27 10:42:22 +0600

    Change data of server to json

--- security/check_first_start.py
+++ security/check_first_start.py
@@ -5,7 +5,7 @@ import json
 def first_start(url):
     response = requests.get(url)
     if response.status_code == 200:
-        response_json = response.text
-        print(response_json)
+        response_json = json.loads(response.text)
+        
 
 first_start('https://xchess.webnet.kz/check-code/?code=testMessage')
